ba7e09f43d378ee458d88ec8f7188ac6fc79114c
Ailya Sayed <ailyasayed64@gmail.com>
2025-04-22 16:11:02 +0100

    Implemented CORS

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors'); // Import the cors package
 
 const app = express();
-//app.use(express.json());
-const port = 8000
+const port = 8000;
+
+// Use cors middleware to enable CORS
+app.use(cors());
+
+// Use express.json() to parse JSON bodies
+app.use(express.json());
 
 // Tell Express that we want to use EJS as the templating engine
 app.set('view engine', 'ejs');
 
-// Set up the body parser 
+// Set up the body parser for URL-encoded data
 app.use(express.urlencoded({ extended: true }));
 
 // Set up public folder (for css and static js)
@@ -24,11 +30,11 @@ const db = mysql.createConnection({
 // Connect to the database
 db.connect((err) => {
   if (err) {
-      throw err
+      throw err;
   }
-  console.log('Connected to database')
-})
-global.db = db
+  console.log('Connected to database');
+});
+global.db = db;
 
 // Load the route handlers
 const mainRoutes = require("./routes/main");
@@ -37,10 +43,5 @@ app.use('/', mainRoutes);
 const apiRoutes = require('./routes/api')(db);
 app.use('/api', apiRoutes);
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 // Start the web app listening
-app.listen(port, () => console.log(`Node app listening on port ${port}!`))
+app.listen(port, () => console.log(`Node app listening on port ${port}!`));
